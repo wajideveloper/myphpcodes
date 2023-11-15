4175d0908a9ddf1896582b93056e362e10b7a241
wajideveloper <wajidullah42540@gmail.com>
2023-11-15 13:53:10 +0500

    my php codes

---
+++ index.php
@@ -0,0 +1,65 @@
+<?php 
+echo"i loved pizza";
+echo"i liked pizza";
+// this my learing phase with php
+
+//vaiable 
+$name = "bro code";
+$food = "pizza";
+$email = "fake@gmail.com";
+
+
+//number integer
+$age = 21;
+$users = 2;
+$quantity = 3;
+
+//float
+$gpa =2.5;
+$price = 4.99;
+
+//boolean
+$employed = true;// for true it will print 1
+$online = false;// for false it will print nothing
+
+//arithemic operation
+$x = 12;
+$y = 12;
+$z = null;
+
+// $z = $x + $y;
+// $z = $x - $y;
+// $z = $x * $y;
+// $z = $x ** $y;
+// $z = $x / $y;
+$z = $x % $y;
+
+echo "the arematic {$z}<br>";
+
+echo"hello {$name}<br>";
+echo"do you like {$food}<br.";
+echo"tell me about your email {$email}<br>";
+
+
+echo"hello {$age}<br>";
+echo"do you like {$users}<br>";
+echo"tell me about your email {$quantity}<br>";
+
+echo"tell me about your gpa: {$gpa}<br>";
+echo"tell me about your price: \${$price}<br>";
+
+echo"tell me about your status : {$online}<br>";
+echo"tell me about your status : {$employed}<br>";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <br>
+    <button>order pizza</button>
+</body>
+</html>
